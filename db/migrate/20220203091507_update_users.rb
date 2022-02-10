da52235a4411ea143450fdc20f487f229eb9d776
RAVINDER CHAUHAN <94591337+ravinderdevohack@users.noreply.github.com>
2022-02-10 16:56:25 +0530

    Remove column name of users

--- db/migrate/20220203091507_update_users.rb
+++ db/migrate/20220203091507_update_users.rb
@@ -1,7 +1,7 @@
 class UpdateUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :name, :string
-    add_column :users, :phone_number, :string
+#     add_column :users, :name, :string
+#     add_column :users, :phone_number, :string
     add_column :users, :avatar, :string
     add_column :users, :user_type, :integer, null:false, default: 0
     add_column :users, :display, :integer, null:false, default: 1
